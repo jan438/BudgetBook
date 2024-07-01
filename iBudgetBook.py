77823a42619ef76bdfbe7bc72008f7fed91eb1f0
jan438 <jan438@users.noreply.github.com>
2024-07-01 16:31:10 +0200

    Drawstring added

--- iBudgetBook.py
+++ iBudgetBook.py
@@ -7,4 +7,5 @@ fName = "aFile.txt"
 path = "C:/Users/janbo/OneDrive/Documents/GitHub/BudgetBook/PDF"
 os.chdir(path)
 c = Canvas("Hello-world.pdf")
+c.drawString(100, 750, "Welcome to Reportlab!")
 c.save()
