77136fe5ce86ebb0c3840b474b37f916c9471fd6
jan438 <jan438@users.noreply.github.com>
2024-07-01 18:12:40 +0200

    textobject added

--- iBudgetBook.py
+++ iBudgetBook.py
@@ -7,5 +7,7 @@ from reportlab.pdfgen.canvas import Canvas
 path = "C:/Users/janbo/OneDrive/Documents/GitHub/BudgetBook/PDF"
 os.chdir(path)
 c = Canvas("Hello-world.pdf")
+textobject = c.beginText()
+textobject.setTextOrigin(2, 2.5*inch)
 c.drawString(100, 750, "Welcome to Reportlab!")
 c.save()
