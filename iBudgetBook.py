5e80ea0259dee3645b59bb4931ac56fa2051b508
jan438 <jan438@users.noreply.github.com>
2024-07-01 11:26:23 +0200

    Removed code

--- iBudgetBook.py
+++ iBudgetBook.py
@@ -1,20 +1,11 @@
 import pytz
 import os
 import sys
-import re
-import math
-import unicodedata
 from pathlib import Path
 from datetime import datetime, date, timedelta
-from ics import Calendar, Event
 from pypdf import PdfReader, PdfWriter
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.lib.pagesizes import LETTER, A4, landscape
-from reportlab.lib.units import inch
-from reportlab.lib.colors import blue, green, black, red, pink, gray, brown, purple, orange, yellow
-from reportlab.pdfbase import pdfmetrics  
-from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.pdfbase.pdfmetrics import registerFontFamily
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Table, Image, Spacer, Frame
-from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.lib.enums import TA_LEFT, TA_RIGHT, TA_CENTER
+
+for i in range(1000):
+    print("Hallo")
