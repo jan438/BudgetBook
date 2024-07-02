8d1822160fe4812b83a0ac9a1c81c76df88a0920
jan438 <jan438@users.noreply.github.com>
2024-07-02 11:17:03 +0200

    bar chart added

--- iBudgetBook.py
+++ iBudgetBook.py
@@ -7,15 +7,38 @@ from reportlab.pdfgen.canvas import Canvas
 from reportlab.platypus import Paragraph,SimpleDocTemplate
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.graphics.charts.piecharts import Pie
-from reportlab.lib.colors import brown,blue
+from reportlab.lib.colors import brown,blue, PCMYKColor, black
 from reportlab.graphics.charts.legends import Legend
 from reportlab.graphics.shapes import Drawing
-from reportlab.lib.colors import black
+from reportlab.graphics.charts.barcharts import VerticalBarChart
 
-path = "C:/Users/janbo/OneDrive/Documents/GitHub/BudgetBook/PDF"
-os.chdir(path)
-c = Canvas("Hello-world.pdf")
-textobject = c.beginText()
-textobject.setTextOrigin(2, 2.5*inch)
-c.drawString(100, 750, "Welcome to Reportlab!")
-c.save()
+def create_bar_graph():
+    d = Drawing(280, 250)
+    bar = VerticalBarChart()
+    bar.x = 50
+    bar.y = 85
+    data = [[1,2,3,None,None,None,5],
+            [10,5,2,6,8,3,5],
+            [5,7,2,8,8,2,5],
+            [2,10,2,1,8,9,5],
+            ]
+    bar.data = data
+    bar.categoryAxis.categoryNames = ['Year1', 'Year2', 'Year3',
+                                      'Year4', 'Year5', 'Year6',
+                                      'Year7']
+    bar.bars[0].fillColor   = PCMYKColor(0,100,100,40,alpha=85)
+    bar.bars[1].fillColor   = PCMYKColor(23,51,0,4,alpha=85)
+    bar.bars.fillColor       = PCMYKColor(100,0,90,50,alpha=85)
+    d.add(bar, '')
+    d.save(formats=['pdf'], outDir='.', fnRoot='test')
+
+#path = "C:/Users/janbo/OneDrive/Documents/GitHub/BudgetBook/PDF"
+#os.chdir(path)
+#c = Canvas("Hello-world.pdf")
+#textobject = c.beginText()
+#textobject.setTextOrigin(2, 2.5*inch)
+#c.drawString(100, 750, "Welcome to Reportlab!")
+#c.save()
+if __name__ == '__main__':
+    create_bar_graph()
+
