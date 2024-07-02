190f455907179b79c99667b2a3904d8820aa38c2
jan438 <jan438@users.noreply.github.com>
2024-07-02 10:32:47 +0200

    Multiple imports

--- iBudgetBook.py
+++ iBudgetBook.py
@@ -1,8 +1,16 @@
 import os
 import sys
+import csv
 from pathlib import Path
 from reportlab.lib.units import inch
 from reportlab.pdfgen.canvas import Canvas
+from reportlab.platypus import Paragraph,SimpleDocTemplate
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.graphics.charts.piecharts import Pie
+from reportlab.lib.colors import brown,blue
+from reportlab.graphics.charts.legends import Legend
+from reportlab.graphics.shapes import Drawing
+from reportlab.lib.colors import black
 
 path = "C:/Users/janbo/OneDrive/Documents/GitHub/BudgetBook/PDF"
 os.chdir(path)
