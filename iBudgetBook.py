8246782dacddee06c71a9bb3f7ed65dc3dc604a1
jan438 <jan438@users.noreply.github.com>
2024-07-03 10:41:16 +0200

    Removed rows

--- iBudgetBook.py
+++ iBudgetBook.py
@@ -19,10 +19,7 @@ def create_bar_graph():
     bar = VerticalBarChart()
     bar.x = 50
     bar.y = 85
-    data = [[1,2,3,None,None,None,5],
-            [10,5,2,6,8,3,5],
-            [5,7,2,8,8,2,5],
-            [2,10,2,1,8,9,5],
+    data = [[1,2,3,None,None,None,5]
             ]
     bar.data = data
     bar.categoryAxis.categoryNames = ['Year1', 'Year2', 'Year3',
