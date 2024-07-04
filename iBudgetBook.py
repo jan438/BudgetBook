f8c95dcd32e82f7591a6f1e87c75d591008b7bb4
jan438 <jan438@users.noreply.github.com>
2024-07-04 14:51:49 +0200

    removed spaces

--- iBudgetBook.py
+++ iBudgetBook.py
@@ -64,6 +64,6 @@ if __name__ == '__main__':
     #    csvreader = csv.reader(file)
     #    for row in csvreader:
     #        if row[0] == "Date":
-    data = [[1,2,3,None,None,5,5,5],[1,2,3,4,5,6,7,8]        ]
+    data = [[1,2,3,None,None,5,5,5],[1,2,3,4,5,6,7,8]]
     create_bar_graph(data)
     create_pie_chart(True)
