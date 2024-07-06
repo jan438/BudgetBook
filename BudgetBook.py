42b30125210f492a52d76097fb969d8215a85c07
jan438 <jan438@users.noreply.github.com>
2024-07-06 13:22:57 +0200

    Tweede piechart added

--- BudgetBook.py
+++ BudgetBook.py
@@ -50,6 +50,22 @@ def create_pie_chart(legend=False):
     d.add(pie)
     d.save(formats=['pdf'], outDir='./Data', fnRoot='pie1')
 
+def pie_chart_with_legend():
+    data = list(range(15, 105, 15))
+    drawing = Drawing(width=400, height=200)
+    my_title = String(170, 40, 'My Pie Chart', fontSize=14)
+    pie = Pie()
+    pie.sideLabels = True
+    pie.x = 150
+    pie.y = 65
+    pie.data = data
+    pie.labels = [letter for letter in 'abcdefg']
+    pie.slices.strokeWidth = 0.5
+    drawing.add(my_title)
+    drawing.add(pie)
+    add_legend(drawing, pie, data)
+    return drawing
+
 def add_legend(draw_obj, chart, data):
     legend = Legend()
     legend.alignment = 'right'
@@ -81,4 +97,14 @@ if __name__ == '__main__':
     data.append([0,0,0,som,0,0,0,0])
     create_bar_graph(data)
     create_pie_chart(True)
+    doc = SimpleDocTemplate('flowable_with_chart.pdf')
+    elements = []
+    styles = getSampleStyleSheet()
+    ptext = Paragraph('Text before the chart', styles["Normal"])
+    elements.append(ptext)
+    chart = pie_chart_with_legend()
+    elements.append(chart)
+    ptext = Paragraph('Text after the chart', styles["Normal"])
+    elements.append(ptext)
+    doc.build(elements)
     key = input("Wait")
