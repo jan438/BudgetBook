7e8a0c3fe5c8686bcd8300ff81824d00ed6445a4
jan438 <jan438@users.noreply.github.com>
2024-07-15 13:38:52 +0200

    prinr removed

--- BudgetBook.py
+++ BudgetBook.py
@@ -63,9 +63,7 @@ def process_transactions(findata, d):
         bookday = int(bookdate[:2])
         bookmonth = int(bookdate[3:5])
         bookyear = int(bookdate[6:10])
-        datebook = str(bookyear) + str(bookmonth) + str(bookday)
         bd = days_since_1990(bookyear, 1, bookday)
-        print(datebook)
         if bookyear <= endyear and bookmonth <= endmonth and bookday <= endday and findata[j][3] != "Begin Saldos":
             output_num = remove_decimal_num(findata[j][5])
             first = False
