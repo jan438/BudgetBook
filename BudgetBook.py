650839bcda3f3728c068302af26ef5f3205994f8
jan438 <jan438@users.noreply.github.com>
2024-09-21 11:39:01 +0200

    print removed

--- BudgetBook.py
+++ BudgetBook.py
@@ -43,10 +43,8 @@ def date_from_days(days):
 
 def begin_saldos(findata):
     for j in range(len(findata)):
-        #print(j,findata[j][0], findata[j][1], findata[j][2], findata[j][3], findata[j][4], findata[j][5], findata[j][6])
         if findata[j][0] == "Transfer" and findata[j][4][:12] == "Begin Saldos":
             account = findata[j][4][17:]
-            print(j,findata[j][0], account, findata[j][3])
             endmonth = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
             MyAccounts.append(Account(account, findata[j][3], endmonth))
     print_myaccounts()
