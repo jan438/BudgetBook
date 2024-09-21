cd0afb9d01407c95053a12ed7c43eb83ccc1ad0f
jan438 <jan438@users.noreply.github.com>
2024-09-21 08:46:32 +0200

    Name changed

--- BudgetBook.py
+++ BudgetBook.py
@@ -175,7 +175,7 @@ def BudgetBookPie(data):
 if __name__ == '__main__':
     path = "C:/Users/janbo/OneDrive/Documents/GitHub/BudgetBook/Data"
     os.chdir(path)
-    file_to_open = "OurBudgetBook.csv"
+    file_to_open = "OurBudgetBookExport.csv"
     count = 0
     som = 0
     findata = []
