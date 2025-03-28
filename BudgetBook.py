fdf19c35865ff7491a7fb2ecd5aa9efae71a7ffa
jan438 <jan438@users.noreply.github.com>
2025-03-28 15:58:41 +0100

    format balance

--- BudgetBook.py
+++ BudgetBook.py
@@ -176,7 +176,7 @@ def create_pie_accounts(data):
     pie.labels = []
     for obj in data:
         pie.data.append(obj.balance)
-        s = str(obj.balance/100)
+        s = "{:.2f}".format(obj.balance / 100)
         pie.labels.append(obj.name + " " + s)
     pie._seriesCount = len(pie.data)
     add_legend(d, pie, pie.data)
